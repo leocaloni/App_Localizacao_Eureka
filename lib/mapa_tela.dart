9fc6d71b7c65d37f31e0bb78eee2e15c5b49a75b
Leonardo Caloni Munduruca <leomunduruca@gmail.com>
2024-05-26 16:12:23 -0300

    nomes dos estandes personalizados

--- lib/mapa_tela.dart
+++ lib/mapa_tela.dart
@@ -14,7 +14,30 @@ class _MapaTelaState extends State<MapaTela> {
         child: Center(
           child: Column(
             children: [
-              SizedBox(height: 50),
+
+              const SizedBox(height: 50),
+
+              Image.asset('lib/images/logo-IMT-SemNome-Branca.png', height: 50),
+
+              const SizedBox(height: 10),
+
+              const Text(
+                'EUREKA 2024',
+                style: TextStyle(
+                    fontSize: 15, fontFamily: 'Roboto Mono', color: Colors.white),
+              ),
+
+              const SizedBox(height: 20),
+
+              Container(
+                width: double.infinity,
+                height: 2.0,
+                color: Colors.white,
+                margin: const EdgeInsets.symmetric(horizontal: 20),
+              ),
+
+              const SizedBox(height: 60),
+
               Container(
                 width: 360, // Largura total do grid
                 height: 360, // Altura total do grid
@@ -25,82 +48,87 @@ class _MapaTelaState extends State<MapaTela> {
                     Positioned(
                       left: 0,
                       top: 0,
-                      child: _buildEstandeWidget(),
+                      child: _buildEstandeWidget('1'),
                     ),
                     Positioned(
                       left: 100,
                       top: 0,
-                      child: _buildEstandeWidget(),
+                      child: _buildEstandeWidget('2'),
                     ),
                      Positioned(
                       left: 200,
                       top: 0,
-                      child: _buildEstandeWidget(),
+                      child: _buildEstandeWidget('3'),
                     ),
                     Positioned(
                       left: 300,
                       top: 0,
-                      child: _buildEstandeWidget(),
+                      child: _buildEstandeWidget('4'),
                     ),
                     Positioned(
                       left: 0,
                       top: 100,
-                      child: _buildEstandeWidget(),
+                      child: _buildEstandeWidget('5'),
                     ),
                     Positioned(
                       left: 100,
                       top: 100,
-                      child: _buildEstandeWidget(),
+                      child: _buildEstandeWidget('6'),
                     ),
                     Positioned(
                       left: 200,
                       top: 100,
-                      child: _buildEstandeWidget(),
+                      child: _buildEstandeWidget('7'),
                     ),
                     Positioned(
                       left: 300,
                       top: 100,
-                      child: _buildEstandeWidget(),
+                      child: _buildEstandeWidget('8'),
                     ),
                     Positioned(
                       left: 0,
                       top: 200,
-                      child: _buildEstandeWidget(),
+                      child: _buildEstandeWidget('9'),
                     ),
                     Positioned(
                       left: 100,
                       top: 200,
-                      child: _buildEstandeWidget(),
+                      child: _buildEstandeWidget('10'),
                     ),
                     Positioned(
                       left: 200,
                       top: 200,
-                      child: _buildEstandeWidget(),
+                      child: _buildEstandeWidget('11'),
                     ),
                     Positioned(
                       left: 300,
                       top: 200,
-                      child: _buildEstandeWidget(),
+                      child: _buildEstandeWidget('12'),
                     ),
                     Positioned(
                       left: 300,
                       top: 300,
-                      child: _buildEstandeWidget(),
+                      child: _buildEstandeWidget('13'),
                     ),
                     Positioned(
                       left: 200,
                       top: 300,
-                      child: _buildEstandeWidget(),
+                      child: _buildEstandeWidget('14'),
                     ),
                     Positioned(
                       left: 100,
                       top: 300,
-                      child: _buildEstandeWidget(),
+                      child: _buildEstandeWidget('15'),
+                    ),
+                    Positioned(
+                      left: 330,
+                      top: 260,
+                      child: _buildPortaWidget(),
                     ),
                     Positioned(
                       left: 0,
                       top: 300,
-                      child: _buildEstandeWidget(),
+                      child: _buildEstandeWidget('16'),
                     ),
                   ],
                 ),
@@ -112,15 +140,29 @@ class _MapaTelaState extends State<MapaTela> {
     );
   }
 
-  Widget _buildEstandeWidget() {
+  Widget _buildEstandeWidget(String estande) {
     return Container(
       width: 60, // Largura do estande
       height: 60, // Altura do estande
       color: Colors.blue, // Cor do estande
+      child: Center(
+        child: Text(
+          estande, // Texto representando o estande
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildPortaWidget() {
+    return Container(
+      width: 30, // Largura do estande
+      height: 40, // Altura do estande
+      color: Colors.red, // Cor do estande
       child: const Center(
         child: Text(
-          'E', // Texto representando o estande
-          style: TextStyle(color: Colors.white),
+          'Porta', // Texto representando o estande
+          style: TextStyle(color: Colors.white, fontSize: 11),
         ),
       ),
     );
